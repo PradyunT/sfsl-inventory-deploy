5e5bf96d34ea1a4d4938f375de0a6df94960d722
Pradyun Tandra <121410281+PradyunT@users.noreply.github.com>
2025-06-12 21:50:09 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -18,21 +18,25 @@ const port = process.env.PORT || 8080;
 const allowedOrigins = [process.env.CLIENT_URL, process.env.DELIVERY_URL];
 
 // Middlewares
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//   })
+// );
+
+// Allow requests from any origin
+// TODO this should be replaced upon full integration with delivery team
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json());
